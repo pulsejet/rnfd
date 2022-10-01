fd8629bef5de94a22fd1e1c6f22232ae533d8cf2
Varun Patil <varunpatil@ucla.edu>
2022-09-30 19:23:19 -0700

    Remove dummy fib

--- src/pipeline/incoming.rs
+++ src/pipeline/incoming.rs
@@ -9,18 +9,6 @@ use crate::table::Table;
 pub fn thread(chan_in: Receiver<Arc<UdpPacket>>, chan_out: Sender<(Vec<u8>, SocketAddr)>) {
     std::thread::spawn(move || {
         let mut table = Table::new(chan_out);
-
-        // Create dummy FIB
-        let name = vec![8, 3, 110, 100, 110, 8, 5, 118, 97, 114, 117, 110]; // /ndn/varun
-        let hop = crate::table::pit::NextHop {
-            addr: std::net::SocketAddr::V4("127.0.0.1:8000".parse().unwrap()),
-            cost: 1,
-        };
-        let e1 = table.pit.insert_or_get(&name);
-        if let Ok((e, _, _)) = e1 {
-            e.borrow_mut().nexthops.push(hop.clone());
-        }
-
         loop {
             let packet = chan_in.recv().unwrap();
             process_packet(&mut table, packet);
